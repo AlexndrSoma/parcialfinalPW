99295910b5ac8fec76d044ea1838dc24bd122066
AlexndrSoma <00404416@uca.edu.sv>
2018-12-01 17:30:37 -0600

    Interfaz grafica basica, CRUD COMPLETO

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
 app.listen(app.get('port') , () => {
- 
+    console.log('EScuchando puerto 3000');
 });
 
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,18 +4,38 @@ const router = express.Router();
 const Task = require('../models/task');
 
 router.get('/', async (req,res) => {
-  /*  const tasks = await Task.find(); */
-    res.render ('index');
+   const tasks = await Task.find(); 
+    res.render ('index', { tasks });
 
 });
- /*
-router.post('/', async (req,res) => {
-    const tasks = await Task.find();
-    res.render ('index', {tasks});
+ 
+router.post('/add', async (req,res) => {
+    const tasks =  new Task(req.body);
+    await tasks.save();
+    res.redirect('/');
 
 });
 
-*/
+router.get('/edit/:id', async (req,res) => {
+    const task = await Task.findById(req.params.id);
+    res.render('edit', { task });
+  });
+  
+  
+  router.post('/edit/:id', async (req,res) => {
+    const { id } = req.params;
+    await Task.update({_id:id}. req.body);
+     res.redirect('/');
+  });
+
+
+router.get('/delete/:id', async (req,res) => {
+    let { id } = req.params;
+    await Task.remove({_id:id});
+    res.redirect('/');
+});
+
+
 
 
 
